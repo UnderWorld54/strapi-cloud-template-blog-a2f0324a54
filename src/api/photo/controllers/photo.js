6139da9aedf014f09bd55d85828c99463e489fb8
SAMINA Angelo <angelosamina@gmail.com>
2025-11-13 19:36:33 +0100

    Add "photos"

--- src/api/photo/controllers/photo.js
+++ src/api/photo/controllers/photo.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  category controller
+ * photo controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController('api::photo.photo');
